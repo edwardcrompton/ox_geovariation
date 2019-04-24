72feeb81ac9c9605cc37a7291d5b65cb81d6a17b
Edward Crompton <edward.crompton@oxfam.org>
2019-04-24 11:45:10 +0100

    366 Adding a module to handle geovariations

---
+++ src/GeoVariations.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * @file
+ *  Contains class GeoVariations.
+ */
+
+namespace Drupal\ox_geovariation;
+
+/**
+ * Class GeoVariations
+ */
+class GeoVariations {
+  
+  /**
+   * Gets the donations link for each country code and exposes them to js.
+   */
+  public function loadDonationLinks() {
+    
+  }
+}
