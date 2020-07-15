4648804beead561204aa41b1fb485c2bbab862eb
Edward Crompton <edward.crompton@oxfam.org>
2020-07-15 14:07:16 +0100

    Refactor js for displaying relevant affiliate details #689

--- js/ox_near_you.js
+++ js/ox_near_you.js
@@ -4,48 +4,61 @@
     attach: function (context, settings) {
 
       $('#block-oxfamnearyou').once('oxfamnearyou').each(function() {
-        // Get the users country code, and show that country
-        var usersCountryCode = oxGeovariation.get();
-        switchAffiliate(usersCountryCode);
 
-        // Create a list of all the available affiliates
-        var affiliateList = [];
+        // Get the user's country code, and show that affiliate initially.
+        var initialAffiliateCode = getInitialAffiliateCode();
+        setAffiliate(initialAffiliateCode);
+
+        // Create an array of all the available affiliates
+        var affiliateArray = [];
         $('.affiliate').each(function() {
           var affiliateTitle = $(this).find('h3').text();
           var affiliateCode = $(this).attr('data-affiliate-country-code');
-          affiliateList[affiliateCode] = affiliateTitle;
+          affiliateArray[affiliateCode] = affiliateTitle;
         })
 
         // Add a select list with the affiliates as options
         var selectElementLabel = '<label for="affiliate-selector" class="inline-block pr-6">' + Drupal.t('Choose an Oxfam affiliate') + '</label>';
-
-        var option;
         var selectElement = $('<select>').prop('id', 'affiliate-selector');
 
-        for (const affiliateCode in affiliateList) {
-          option = $("<option>").prop('value', affiliateCode).text(affiliateList[affiliateCode])
+        for (const affiliateCode in affiliateArray) {
+          var option = $("<option>").prop('value', affiliateCode).text(affiliateArray[affiliateCode])
+          if (affiliateCode === initialAffiliateCode) {
+            option.prop('selected', true);
+          }
           selectElement.append(option);
         }
-        selectElement.val(usersCountryCode);
         $('#block-oxfamnearyou .near-you .affiliate:first').before(selectElementLabel).before(selectElement);
 
         // Add a listener for our new select list to alter the visible fields when selected
         $('#affiliate-selector').change(function() {
           chosenAffiliateCode = $(this).val();
-          switchAffiliate(chosenAffiliateCode);
-        })
-      })
+          setAffiliate(chosenAffiliateCode);
+        });
+      });
     }
-  }
+  };
 
-  function switchAffiliate(affiliateCode) {
-    // Check whether affiliate is a valid one (if not, default to 'OI')
-    if ($('*[data-affiliate-country-code=' + affiliateCode + ']').length == 0) {
-      affiliateCode = 'OI';
-    }
-    // Hide all the affiliates
+  /**
+   * Sets the visible affiliate.
+   */
+  function setAffiliate(affiliateCode) {
+     // Hide all the affiliates and select only the relevant one.
     $('.affiliate').hide();
     $('*[data-affiliate-country-code=' + affiliateCode + ']').show();
+  }
+
+  /**
+   * Returns the relevant affiliate code for the location of the user.
+   */
+  function getInitialAffiliateCode() {
+    var countryCode = oxGeovariation.get();
+
+    // Check whether country code corresponds to an affiliate.
+    if ($('*[data-affiliate-country-code=' + countryCode + ']').length == 0) {
+      return 'OI';
+    }
+    return countryCode;
   }
 
 })(jQuery, Drupal);
